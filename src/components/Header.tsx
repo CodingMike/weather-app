d7977f1da49be3146da65fd761959c7b776ef824
Mike <michaelmaratheftis@hotmail.com>
2023-06-15 16:35:56 +0100

    Add existing project files to Git

---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+const Header = (): JSX.Element => (
+  <>
+    <h1 className="text-4xl font-thin">
+      Weather <span className="font-black">Forecast</span>
+    </h1>
+    <p className="text-sm mt-2">Enter a city below.</p>
+  </>
+);
+
+export default Header;
